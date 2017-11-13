566799565a774f14077f36212a28d624bd0c3656
XIAOLONG LI <xiaolongli.ny@gmail.com>
2017-11-13 12:28:11 +0000

    Add patials and destroy action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -43,10 +43,19 @@ class ArticlesController < ApplicationController
         @article = Article.find(params[:id])
     end
     
+    def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "Artcile was successfully delated"
+    redirect_to articles_path
+    end
+    
     private
         def article_params
             params.require(:article).permit(:title, :description)
         end
+        
+   
     
     
 
