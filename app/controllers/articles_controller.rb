2917df596dbca39648848fae8656d1ba314d3f9b
XIAOLONG LI <xiaolongli.ny@gmail.com>
2017-11-13 11:49:48 +0000

    edit, update,show,index listing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,20 @@
 class ArticlesController < ApplicationController
 
+
+    def index
+        @articles = Article.all
+    end
+
     def new
         @article = Article.new
         
     end
+    
+    def edit
+        @article = Article.find(params[:id])
+    end
+    
+  
     
     def create
         #render plain:params[:article].inspect
@@ -15,6 +26,16 @@ class ArticlesController < ApplicationController
             redirect_to article_path(@article)
         else
             render 'new'
+        end
+    end
+    
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+            flash[:notice] = "Article was successfully"
+            redirect_to article_path(@article)
+        else
+            render 'edit'
         end
     end
     
