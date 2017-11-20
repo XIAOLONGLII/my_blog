84ec90a810bda619787a49a44870457fd15ff4d3
XIAOLONG LI <xiaolongli.ny@gmail.com>
2017-11-20 13:08:37 +0000

    add association beween user and articles and addd debugger view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,7 +19,9 @@ class ArticlesController < ApplicationController
     
     def create
         #render plain:params[:article].inspect
+        
         @article = Article.new(article_params)
+        @artcile.user = user.first
         #@article.save
         #redirect_to article_path(@article)
         if @article.save
